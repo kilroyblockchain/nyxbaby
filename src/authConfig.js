fcaf3133497dca0a96a79cf90db2866a53524b2d
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-12 11:26:32 -0600

    signon added

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,9 +3,9 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "cecf9ab2-8682-4ad6-9f85-3812fe5efba0",
+        clientId: "2be92905-5f2c-49bb-a1ba-3664388dd3a8",
         authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
-        redirectUri: "https://dev-my.file.baby"
+        redirectUri: "https://my.file.baby"
     },
     cache: {
         cacheLocation: "localStorage",
