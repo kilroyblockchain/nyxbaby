6baa4cff511564f3b4fe7432637524ebf5d1bca4
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-16 17:30:58 -0600

    fix lint errors

--- src/FileUploadPage.js
+++ src/FileUploadPage.js
@@ -3,10 +3,7 @@ import React, { useState, useRef, useEffect, useCallback } from 'react';
 function FileUploadPage({ userName }) {
     const [manifestFile, setManifestFile] = useState(null);
     const [imageFile, setImageFile] = useState(null);
-    const [imageFileName, setImageFileName] = useState('');
-    const [imageFileType, setImageFileType] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const [uploadResponse, setUploadResponse] = useState(null);
     const [error, setError] = useState('');
     const [savedToFileBaby, setSavedToFileBaby] = useState(false);
 
@@ -24,16 +21,39 @@ function FileUploadPage({ userName }) {
     const resetAllStates = useCallback(() => {
         setManifestFile(null);
         setImageFile(null);
-        setImageFileName('');
-        setImageFileType('');
-        setUploadResponse(null);
-        setSavedToFileBaby(false);
-        setError('');
         setIsLoading(false);
+        setError('');
+        setSavedToFileBaby(false);
         resetFileInputs();
     }, [resetFileInputs]);
 
-    // ... [rest of your event handlers]
+    const handleManifestFileChange = (event) => {
+        setManifestFile(event.target.files[0]);
+    };
+
+    const handleImageFileChange = (event) => {
+        setImageFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        setError('');
+        // Add your file upload logic here
+        // Remember to use manifestFile and imageFile in your logic
+
+        // Example: console.log(manifestFile, imageFile);
+        // Reset the form and state after handling the submission
+        resetAllStates();
+    };
+
+    const handleSaveToFileBaby = async () => {
+        // Add your logic to save to File Baby here
+        // Example: console.log('Saving to File Baby');
+        // Update the state as needed after saving
+        setSavedToFileBaby(true);
+        resetAllStates();
+    };
 
     useEffect(() => {
         resetAllStates();
@@ -42,7 +62,35 @@ function FileUploadPage({ userName }) {
     return (
         <div>
             <h1>2. Upload Manifest and Image</h1>
-            {/* ... [rest of your component's JSX] */}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="manifestFile">Manifest File:</label>
+                    <input
+                        ref={manifestFileInput}
+                        id="manifestFile"
+                        type="file"
+                        onChange={handleManifestFileChange}
+                        accept=".json"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="imageFile">Image File:</label>
+                    <input
+                        ref={imageFileInput}
+                        id="imageFile"
+                        type="file"
+                        onChange={handleImageFileChange}
+                        accept="image/jpeg,image/png"
+                    />
+                </div>
+                <button type="submit" disabled={isLoading}>
+                    Upload Files
+                </button>
+            </form>
+
+            {isLoading && <p>Uploading...</p>}
+            {error && <p className="error">{error}</p>}
+            {savedToFileBaby && <p>Image saved to File Baby successfully!</p>}
         </div>
     );
 }
