7f87a069462c1b00413648a86925851e51efde2c
kilroyblockchain <karen@kilroyblockchain.com>
2023-12-22 08:09:39 -0600

    auth config

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,9 +3,12 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "2be92905-5f2c-49bb-a1ba-3664388dd3a8",
+        clientId: "ab50efea-8566-4d5f-a0e6-ca5e869f0f53",
         authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
-        redirectUri: "https://my.file.baby"
+        redirectUri: "https://dev2-my.file.baby"
+//        clientId: "2be92905-5f2c-49bb-a1ba-3664388dd3a8",
+//        authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
+//        redirectUri: "https://my.file.baby"
 //        clientId: "cecf9ab2-8682-4ad6-9f85-3812fe5efba0",
 //        authority: "https://login.microsoftonline.com/73bc0c96-da02-4596-acb7-953c57ed55ac",
 //       redirectUri: "https://dev-my.file.baby"
