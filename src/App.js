afe974929bd9973a536b79c18f34489a5caa09a8
kilroyblockchain <karen@kilroyblockchain.com>
2024-02-17 06:45:39 -0600

    sign in button microsoft

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function SignInButton() {
         });
     };
 
-    return <button onClick={handleLogin}><img src={SignInImage} alt={"Sign in with Microsoft"} />Sign In></button>;
+    return <button onClick={handleLogin}><img src={SignInImage} alt={"Sign in to File Baby with Microsoft"} /></button>;
 }
 
 function AppContent() {
